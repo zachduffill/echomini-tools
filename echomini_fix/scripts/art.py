09a4b85b9ed9e8740c71bc15fe9a07b086857086
zachduffill <zach.duffill@gmail.com>
2026-01-11 15:37:43 +0100

    Embedded art loader

--- echomini_fix/scripts/art.py
+++ echomini_fix/scripts/art.py
@@ -1,10 +1,66 @@
+from mutagen import File
+from mutagen.id3 import APIC
+from mutagen.flac import Picture
+from mutagen.mp4 import MP4Cover
+from mutagen.oggvorbis import OggVorbis
+
 from PIL.Image import Resampling
-from mutagen.id3 import ID3, APIC
 from io import BytesIO
 from PIL import Image
 
 def fix(path):
     pass
+
+def _load_embedded_art(path):
+    audio = File(path)
+    if audio is None: return None
+
+    cls = audio.__class__.__name__
+
+    match cls:
+        case 'MP3' | 'ID3':
+            print("File is of type MP3/ID3")
+            for frame in audio.values():
+                if isinstance(frame, APIC):
+                    print("Found Art!")
+                    return frame.mime, frame.data
+
+            print("No Art found")
+            return None
+        case 'FLAC':
+            print("File is of type FLAC")
+            if audio.pictures:
+                pic = audio.pictures[0]
+                print("Found Art!")
+                return pic.mime, pic.data
+
+            print("No Art found")
+            return None
+        case 'MP4':
+            print("File is of type M4A")
+            covers = audio.get("covr")
+            if covers:
+                cover = covers[0]
+                mime = "image/jpeg" if cover.imageformat == MP4Cover.FORMAT_JPEG else "image/png"
+                print("Found Art!")
+                return mime, bytes(cover)
+
+            print("No Art found")
+            return None
+
+    if isinstance(audio, OggVorbis):
+        print("File is of type OGG")
+        if audio.pictures:
+            pic = audio.pictures[0]
+            print("Found Art!")
+            return pic.mime, pic.data
+
+        print("No Art found")
+        return None
+
+    print("Invalid Format")
+    return None
+
 
 def _resize_image(data, new_size):
     img = Image.open(BytesIO(data))
