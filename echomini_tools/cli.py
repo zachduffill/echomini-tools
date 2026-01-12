b1663c5bfb68ce795cb6edcc294f76dc81d74dd7
zachduffill <zach.duffill@gmail.com>
2026-01-12 18:11:13 +0100

    changed CLI -h

--- echomini_tools/cli.py
+++ echomini_tools/cli.py
@@ -3,15 +3,15 @@ from echomini_tools.scripts import art, lrc, flac
 
 def run_cli(args):
     parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "command",
-        choices=["art, lrc, flac"],
-        help="which tool to run"
-    )
-    parser.add_argument(
-        "target",
-        help="target file (flac,mp3,ogg,m4a)"
-    )
+    cmd = parser.add_subparsers(dest="command", required=True)
+
+    flac_cmd = cmd.add_parser("flac", help="Fix FLAC files")
+    flac_cmd.add_argument("target", help="target file (.flac)")
+    art_cmd = cmd.add_parser("art", help="Normalise album art")
+    art_cmd.add_argument("target", help="target file (.flac .mp3 .ogg .m4a)")
+    lrc_cmd = cmd.add_parser("lrc", help="Grab song lyrics")
+    lrc_cmd.add_argument("target", help="target file")
+
     parsed = parser.parse_args(args)
 
     match parsed.command:
